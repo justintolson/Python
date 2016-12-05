d106a60fafee7c8f66e39b7e5e2191b87740532c
justintolson <tolsonj29@gmail.com>
2016-12-05 12:09:30 -0800

    Add files via upload

---
+++ LMS Step36.py
@@ -0,0 +1,71 @@
+#1. Assign an integer to a variable
+number = 7
+
+#2. Assign a string to a variable
+string = "Hello World!"
+
+#3. Assign a float to a variable
+number_1 = 3.45
+
+#4. Use the print function and .format() notation to print out the variable you assigned
+print("{} {} {}".format(number, string, number_1))
+
+#5. Use each of these operators +, - , * , / , +=, ­= , %
+x = 2 + 2
+y = 2 - 2
+z = 2 * 2
+q = 2 / 2
+counter = 0
+counter += 1
+counter -= 1
+p = 2 % 4
+
+#6. Use of logical operators: and, or, not
+if number = 3 or number = 6:
+    print("True")
+
+if number = 3 and number > x:
+    print("True")
+
+if number not x:
+    print("False")
+
+
+#7. Use of conditional statements: if, elif, else
+if number >= number_1:
+    print("True")
+elif number_1 > 0:
+    print("True")
+else:
+    print("False")
+
+#8. Use of a while loop
+a, b = 2, 4
+while b < 100:
+    print("Under 100")
+
+#9. Use of a for loop
+list = [1, 2, 3, 4, 5]
+for number in list:
+    print(number + 1)
+
+#10. Create a list and iterate through that list using a for loop to print each item out on a new line
+list = [2, 4, 6, 8]
+for number in list:
+    print("This is the number ", + number)
+
+
+#11. Create a tuple and iterate through it using a for loop to print each item out on a new line
+tuple = (5, 6, 7, 8)
+for number in tuple:
+    print("This is the number ", + number)
+
+
+#12. Define a function that returns a string variable
+def letter():
+    words = "This is a string."
+    return words
+
+
+#13. Call the function you defined above and print the result to the shell
+print(letter())
